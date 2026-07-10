5a34a9f6b8923ee1bf0bfb1a666439ced4be9e6b
MichaelChyzhevskiy <it.nefrick@gmail.com>
2026-07-10 18:59:42 +0300

    MongoDB Connected

--- bootcamp/sprint-01/day-08-contact-keeper/server.js
+++ bootcamp/sprint-01/day-08-contact-keeper/server.js
@@ -1,5 +1,10 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
