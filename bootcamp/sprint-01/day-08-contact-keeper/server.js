ffd36a9ba23bdcea7120630340ebc52c23b0a881
MichaelChyzhevskiy <it.nefrick@gmail.com>
2026-07-13 19:17:37 +0300

    added users register and jwt token

--- bootcamp/sprint-01/day-08-contact-keeper/server.js
+++ bootcamp/sprint-01/day-08-contact-keeper/server.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const connectDB = require('./config/db');
+const app = express();
 
 // Connect Database
 connectDB();
-const app = express();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,9 +18,9 @@ app.get('/', (req, res) => {
 
 
 //Define routes
-app.use('/api/users', require('./routes/usersRouts'));
-app.use('/api/contacts', require('./routes/contactsRouts'));
-app.use('/api/auth', require('./routes/authRouts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
